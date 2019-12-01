72291f1f212a75839c67525cff29c4ab667e3b48
yukivkaras <497321931@qq.com>
2019-12-01 17:36:41 +0800

    cuold be find

--- good.py
+++ good.py
@@ -1,23 +1,51 @@
 #!/usr/bin/python
-from flask import Flask,request
+from flask import Flask, request, render_template
 from flask_cors import CORS
+import json, os
+app = Flask(__name__,static_folder="./templates/static")
 
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    tiantian = open('./kaixin.txt','r',encoding='UTF-8')
-    return tiantian.read()
 
-@app.route('/a',methods=["POST"])
-def a():
-    name = request.get_json()
-    print(name)
-    kaixin = open('./kaixin.txt','w',encoding='UTF-8')
-    kaixin.write(name)
-    kaixin.close()
-    return name
+@app.route('/find_restuarant', methods=["GET"])
+def find_restuarant():
+    return render_template('index.html')
+
+
+@app.route('/find_restuarants', methods=["GET"])
+def get_address():
+    table = open('./database', 'r', encoding='UTF-8').read()
+    table_json = json.loads(table)
+    address_all = ""
+    for i in table_json["datas"]:
+        address = i["restaurant_name"]
+        address_all += " " + address
+    return address_all
+
+
+@app.route('/add_new_address', methods=["POST"])  # 添加新地址-餐厅
+def add_new_address():
+    datas = request.get_json()
+    address = datas['address']
+    restaurant = datas['restaurant']
+    new_data = {"datas": [{
+        "address_id": 4,
+        "restaurant_id": 3,
+        "address_name": address,
+        "restaurant_name": restaurant,
+        "ate_count": 10,
+        "create_time": " 2019/11/14",
+        "last_ate_time": ""
+    }]
+    }
+    print(datas)
+    print(address)
+    print(restaurant)
+    new_address = open('./database', 'a', encoding='UTF-8')
+    new_address.write(json.dumps(new_data))
+    new_address.close()
+    return 'good'
+
 
 if __name__ == "__main__":
     CORS(app, supports_credentials=True)
-    app.run(debug=True,host="192.168.2.107",port=8080)
+    app.run(debug=True, port=8080)
---
+++ templates/static/precache-manifest.50dc4dcf99d5f7f35a8761e3fe9f2019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776b59af8819849dddd0fe0118bd6db8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dba5ea814390c4b020ee",
+    "url": "./static/css/2.f69183c5.chunk.css"
+  },
+  {
+    "revision": "2d3cd5f7013e393cc061",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "dba5ea814390c4b020ee",
+    "url": "./static/js/2.354280c5.chunk.js"
+  },
+  {
+    "revision": "2d3cd5f7013e393cc061",
+    "url": "./static/js/main.e7949e40.chunk.js"
+  },
+  {
+    "revision": "9e8e0da013432bbd6579",
+    "url": "./static/js/runtime-main.4e9833e8.js"
+  }
+]);
